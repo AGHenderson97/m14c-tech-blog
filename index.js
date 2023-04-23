2fee75028cf4dc1b58f5def99e3626aed1cd7405
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-23 01:02:35 -0400

    added view directory and edited code in index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+// Set up middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up static file serving
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up routes
+const routes = require('routes.js');
+app.use('/', routes);
+
+// Set up view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
 });
-
-const routes = require('./routes');
-app.use('/', routes);
