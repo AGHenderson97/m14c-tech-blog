9be2ce8b71543b1395c54b7b82638bb8a98aefc9
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-23 00:54:06 -0400

    json & handlebars

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
 });
+
+const routes = require('./routes');
+app.use('/', routes);
